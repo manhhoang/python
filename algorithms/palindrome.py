88529d904d56f92a605edfdd8fea2afcfa598cb1
Mike Ha <hamanhhoang1102@gmail.com>
2016-01-21 17:48:38 +0800

    udpate

---
+++ algorithms/palindrome.py
@@ -0,0 +1,15 @@
+def palindrome(s):
+    i = 0
+    j = len(s) - 1
+    while i <= j:
+        if s[i] == s[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
+
+print(palindrome("abba"))
+print(palindrome("abbaa"))
+
+
