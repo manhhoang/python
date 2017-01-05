1d212c9d8ad2ff6c653391ccb4f9519a2312972d
Manh Hoang <hamanhhoang1102@gmail.com>
2017-01-05 12:29:30 +0100

    DataFrame

--- codes/Pandas/DataFrame.py
+++ codes/Pandas/DataFrame.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-d = {'one': pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
-     'two': pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
+d = {'one': [1., 2., 3., 4.],
+     'two': [4., 3., 2., 1.]}
 df = pd.DataFrame(d)
 print(df)
 
