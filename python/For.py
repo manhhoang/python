80c439a5faf83f1c30ecb41b0b145f22200defda
Manh Hoang Ha <manhhoang.ha@qrious.co.nz>
2020-09-29 11:43:08 +1300

    Updated

--- python/For.py
+++ python/For.py
@@ -1,13 +1,40 @@
 for i in range(5):
     print(i, end=",")
-
 print('\n')
 
-for i in range(1,5):
+
+for i in range(1, 5):
     print(i, end=' ')
-
 print('\n')
+
 
 for animal in ["dog", "cat", "mouse"]:
-    # You can use format() to interpolate formatted strings
-    print("{} is a mammal".format(animal))
+    print(f"{animal} is a mammal")
+print('\n')
+
+
+a_dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}
+for key in a_dict:
+    print(key)
+print('\n')
+
+
+for key in a_dict.keys():
+    print(key)
+print('\n')
+
+
+for value in a_dict.values():
+    print(value)
+print('\n')
+
+
+for item in a_dict.items():
+    print(item)
+    print(type(item))
+print('\n')
+
+
+for key, value in a_dict.items():
+    print(key, '->', value)
+print('\n')
--- python/Sort.py
+++ python/Sort.py
@@ -1,16 +1,60 @@
 a = [3, 6, 8, 2, 78, 1, 23, 45, 9]
 a.sort()
 print(a)
+print('\n')
+
 
 a.sort(reverse=True)
 print(a)
+print('\n')
 
 
 x = {1: 'f', 3: 'c', 4: 'a', 2: 'b', 0: 'i'}
 y = {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
 print(y)
+print('\n')
 
 
 x = {1: 'f', 3: 'c', 4: 'a', 2: 'b', 0: 'i'}
 y = {k: v for k, v in sorted(x.items(), key=lambda item: item[0])}
 print(y)
+print('\n')
+
+
+class Student:
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
+    def __repr__(self):
+        return repr((self.name, self.grade, self.age))
+student_objects = [
+    Student('john', 'A', 15),
+    Student('jane', 'B', 12),
+    Student('dave', 'B', 10),
+]
+print(sorted(student_objects, key=lambda student: student.age, reverse=True))
+print('\n')
+
+
+student_tuples = [
+    ('john', 'A', 15),
+    ('jane', 'B', 12),
+    ('dave', 'B', 10),
+    ('anna', 'C', 10),
+]
+print(sorted(student_tuples, key=lambda student: (student[2], student[0])))
+print('\n')
+
+
+orders = {
+    'cappuccino': 54,
+    'latte': 56,
+    'espresso': 72,
+    'americano': 48,
+    'cortado': 41
+}
+sort_orders = sorted(orders.items(), key=lambda x: x[1], reverse=True)
+for i in sort_orders:
+    print(i[0], i[1])
+print('\n')
